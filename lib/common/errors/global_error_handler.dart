d6b3156b951d8bf39fb753c5945582981ff8f54e
Vasil Mihaylov <vasil.mihailov22@gmail.com>
2024-09-07 17:31:54 +0300

    Add reference id on teh message and in the log

--- lib/common/errors/global_error_handler.dart
+++ lib/common/errors/global_error_handler.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:quiz_maker/components/text_field.dart';
+import 'package:uuid/uuid.dart';
 import '../../main.dart';
 
 class GlobalErrorHandler extends StatefulWidget {
@@ -14,15 +15,16 @@ class GlobalErrorHandler extends StatefulWidget {
 class _GlobalErrorHandlerState extends State<GlobalErrorHandler> {
 
   void onError(FlutterErrorDetails errorDetails) {
-    logger.e('Caught error: ${errorDetails.exception}');
+    var uuid = const Uuid();
+    String uniqueId = uuid.v4();
+    logger.e('Caught error UUID: $uniqueId, error: ${errorDetails.exception}');
 
     showDialog(
       context: context,
       builder: (BuildContext context) {
         return AlertDialog(
           title: const MyTextField(text: 'Грешка', fontSize: 20,),
-          content: const MyTextField(text:
-            'Нещо се обърка. Моля опитайте отново. Екипът работи по отстраняване на проблема.',
+          content: MyTextField(text: 'Нещо се обърка. Екипът работи по отстраняване на проблема. [$uniqueId]',
           ),
           actions: [
             TextButton(
